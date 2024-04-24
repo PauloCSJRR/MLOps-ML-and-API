c2670cf9848e026efd656749273d91cef7829ec5
PauloCSJR <PAULOCSDASILVAJR@GMAIL.COM>
2024-04-24 16:43:25 -0300

    Sentiment analysis test

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from textblob import TextBlob
 
-app = Flask('my_app')
+app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     return 'My API'
 
-app.run()
+@app.route('/sentiment/<text>')
+def sentiment(text):
+    tb = TextBlob(text)
+    
+    polarity = tb.sentiment.polarity
+    
+    return f'Polarity: {polarity}'
+
+if __name__ == "__main__":
+    app.run(debug=True)
